9227efaeb9dc6c36b836fedf676c24b40f73fb21
wangyi12345 <sfpd456@163.com>
2017-09-12 16:20:57 +0800

    jiujiuchenfabiao

---
+++ wang.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+print("你好");print("世界");
+a=3
+if(a>0):
+    print("我是人")
+else:
+    print("我是神")
+
+    # !/usr/bin/env python
+    # 9 * 9
+for i in range(1, 10):
+    print
+    for j in range(1, i + 1):
+        print "%d*%d=%d" % (i, j, i * j),
+# !/usr/bin/env python
+for i in range(1,10):
+    print
+    for j in range(1,i+1):
+        print "%d*%d=%d" % (i , j, i * j),
